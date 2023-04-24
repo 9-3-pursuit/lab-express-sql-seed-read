1425973de2d78eff72305a64b50307f715dff658
amber-king <amberking@pursuit.org>
2023-04-24 13:37:39 -0400

    added scheme & seed sql files, work in progress

---
+++ back-end/db/seed.sql
@@ -0,0 +1,25 @@
+\ c songs_dev
+INSERT INTO
+    songs (name, artist, album, time, is_favorite)
+VALUES
+    (
+        'Nothing Like Loving You',
+        'Amerie',
+        'All I Have',
+        '4',
+        'true'
+    ),
+    (
+        'Eyes on it',
+        'PARTYNEXTDOOR',
+        'Partymobile',
+        '4',
+        'true'
+    ),
+    (
+        'Rapture',
+        'iiO',
+        'Rapture',
+        '4',
+        'true'
+    );
