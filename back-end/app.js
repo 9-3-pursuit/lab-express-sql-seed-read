9f33bbc11bb7c28d670fe52c7f0e0aa2c896f567
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-24 16:39:27 -0400

    Add db and queries folder and edit app.js

---
+++ back-end/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Your Music Playlist");
+});
+
+
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page Not Found");
+})
+
+
+module.exports = app;
---
+++ back-end/controllers/songController.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const songs = express.Router();
+
+// index
+songs.get("/", (req, res) => {
+
+});
+
+
+module.exports = bookmarks;
