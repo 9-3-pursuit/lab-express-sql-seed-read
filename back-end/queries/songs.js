1f4c8aa8c8e5877325a48e391edc822b77ed7270
SangUn Park <Sangunpark1@gmail.com>
2023-04-25 13:09:33 -0400

    lab part1-2 complete

---
+++ back-end/queries/songs.js
@@ -0,0 +1,40 @@
+
+const db = require('../db/dbConfig.js')
+
+const getAllSongs = async () => {
+    try {
+        const allSongs = await db.any("SELECT * FROM songs");
+        return allSongs;
+    }catch(error){
+        return error
+    }
+};
+
+const getOneSong = async (id) => {
+    try {
+        const oneSong = await db.one("SELECT * FROM songs WHERE id=$1", id);
+        return oneSong;
+    }catch(error){
+        return {error: error}
+    }
+}
+
+const createSong = async (song) => {
+    try {
+     const newSong = await db.one(
+         `INSERT INTO songs(name, artist, album, time, is_favorite) 
+         VALUE ($1, $2, $3, $4, $5) RETURNING * `,
+          [song.name, song.artist, song.album, song.time, song.is_favorite]);
+          return newSong
+ } catch (error) {
+     return { error: error };
+ }
+ }
+
+
+
+module.exports = {
+    getAllSongs,
+    getOneSong,
+    createSong
+}
