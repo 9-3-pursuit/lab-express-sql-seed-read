ae1d30c1487548a7f610d3a4caef495b98769621
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-04-25 11:27:56 -0400

    fixed bug

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = require("./back-end/controllers/songController.js");
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//routes HOME
+app.get("/", (req, res) => {
+  res.send("Tuner App");
+});
+//ROUTES
+app.use("/songs", router);
+//404
+app.get("*", (req, res) => {
+  res.send({ error: "Page Not Found" });
+});
+
+module.exports = app;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
+
 function App() {
   return (
     <div className="">
       <h1>Tuner App</h1>
     </div>
-  );
+
+);
 }
 
 export default App;
