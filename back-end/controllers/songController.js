4cb766cce21bf063acb9f6ea0105f9378d07deb4
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-25 01:24:37 -0400

    add dbconfig

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const songs = express.Router();
+const { getAllSongs } = require("../queries/songs.js");
 
-songs.get("/", (req, res) => {
-  res.json({ status: "ok" }); // index page
+// index page
+songs.get("/", async (req, res) => {
+    //http://localhost:3003/songs
+    const allSongs = await getAllSongs();
+    if (allSongs[0]) {
+      res.status(200).json(allSongs);
+    } else {
+      res.status(500).json({ error: "server error" });
+    }
 });
 
 module.exports = songs;
+
---
+++ back-end/db/seed.sql
@@ -0,0 +1,6 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('My heart will go on','Celine Dion', 'Titanic', '4:42', true),
+('Let it Go', 'Idina Menzel', 'Frozen', '3:52', true),
+('Happy', 'Pharrell Williams', 'Girl', '4:03', true);
