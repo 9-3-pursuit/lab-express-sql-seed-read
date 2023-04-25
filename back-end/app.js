faaa21ee9a517f7bebe4620bed5cbf9dbfb456f2
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-24 23:54:30 -0400

    add schema.sql

---
+++ back-end/app.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require("express");
+const songController = require("./controllers/songController");
+const cors = require("cors");
+
+// CONFIGURATION
+const app = express();
+app.use(cors());
+app.use(express.json()); // json middleware parses incoming requests with JSON payloads
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to the Tuner Full  Application");
+});
+
+app.use("/songs", songController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Sorry, page not found!");
+});
+
+// EXPORT
+module.exports = app;
