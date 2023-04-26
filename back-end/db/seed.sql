476b8561991774f68705075a4afab1e7f43b7935
Ryan Lundy <rlundy1792@gmail.com>
2023-04-26 04:09:52 -0400

    Revert "'Index', 'show', 'create' functionality setup"
    This reverts commit 621cc70b45c366029447fee8c0c5009afd5f02aa.

--- back-end/db/seed.sql
+++
@@ -1,6 +0,0 @@
-\c songs_dev;
-
-INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
- ('Nights', 'Frank Ocean', 'Blonde', '5:07', true),
- ('16', 'Baby Keem', 'The Melodic Blue', '2:36',  true),
- ('Breathe Deeper', 'Tame Impala', 'The Slow Rush', '6:13', true);
