9b62c6b5fb9b802180f438f6c4e55e8401b36d0c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-25 23:20:20 -0400

    create one song

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -25,5 +25,14 @@ songs.get("/:id", async (req, res) => {
   }
 });
 
+// CREATE
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
 
 module.exports = songs;
--- back-end/queries/queries.js
+++ back-end/queries/queries.js
@@ -22,12 +22,12 @@ const getOneSong = async (id) => {
 const createSong = async (song) => {
   try {
     const newSong = await db.one(
-      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      "INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [song.name, song.artist, song.album, song.time, song.is_favorite]
     );
     return newSong;
   } catch (error) {
-    return error;
+      return error;
   }
 };
 
