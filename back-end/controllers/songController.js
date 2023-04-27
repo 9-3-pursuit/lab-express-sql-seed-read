c661f2cbbf7fc7a1e2160735e9acc2af4f698bf0
Keeanu Rodriguez <keeanu7@gmail.com>
2023-04-26 21:25:04 -0400

    added index and create

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs.js")
+const { getAllSongs, getSong, createSong } = require("../queries/songs.js")
 
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
     if (!allSongs.error) {
-      console.log(allSongs)
       res.status(200).json(allSongs);
     } else {
       res.status(500).json({ error: "server error" });
     }
 });
 
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (!song.error) {
+    console.log(song);
+    res.status(200).json(song);
+  } else if (song.error.code === 0) {
+    req.status(404).json({ error: "server not found "})
+  } else {
+    res.status(500).json({ error: "not found" });
+  }
+});
 
+songs.post("/", async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error})
+  }
+});
 
 module.exports = songs;
--- back-end/queries/songs.js
+++ back-end/queries/songs.js
@@ -8,5 +8,31 @@ const getAllSongs = async () => {
       return {error: error};
     }
 };
+
+const getSong = async (id) => {
+  try {
+    const oneSong = await db.oneOrNone("SELECT * FROM songs WHERE id=$1", id);
+    console.log(oneSong)
+    return oneSong;
+  } catch (error) {
+    return {error: error}
+  }
+}
+
+const createSong = async (song) => {
+  try {
+    const newSong = await db.one(
+      "INSERT INTO song (name, artist, album, time, is_favorite)", 
+      [song.name, song.artist, song.album, song.time, song.is_favorite]
+    );
+    return newSong;;
+  } catch (error) {
+    throw (error);
+  }
+}
   
-module.exports = { getAllSongs };
+module.exports = { 
+  getAllSongs,
+  getSong,
+  createSong
+ };
