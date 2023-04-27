babb145a54eb422ea775f563b55f7ed947b79e1f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-26 23:45:12 -0400

    pass all tests in backend

--- back-end/queries/queries.js
+++ back-end/queries/queries.js
@@ -45,9 +45,10 @@ const deleteSong = async (id) => {
 
 const updateSong = async (id, song) => {
   try {
+    const {name, artist, album, time, is_favorite} = song
     const updatedSong = await db.one(
       "UPDATE songs SET name=$1, artist=$2, album=$3, time=$4,is_favorite=$5 WHERE id=$6 RETURNING *",
-      [song.name, song.artist, song.album, song.time, song.is_favorite, id]
+      [name, artist, album, time, is_favorite, id]
     );
     return updatedSong;
   } catch (error) {
