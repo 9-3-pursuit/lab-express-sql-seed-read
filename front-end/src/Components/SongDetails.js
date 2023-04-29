7f95130625fd5df53e7f30f6fff69027d1d401c9
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-29 03:46:26 -0400

    completed songs details

---
+++ front-end/src/Components/SongDetails.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL
+ 
+function SongDetails() {
+  const [song, setSong] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${API}/songs/${id}`).then((response) => {
+      console.log(response.data)
+      setSong(response.data)
+    }).catch((e) => {
+      console.warn("catch:", e)
+    })
+  }, [id])
+
+  const handleDelete = () => {
+    // console.log("I clicked delete");
+    deleteSong();
+  };
+
+  const deleteSong = () => {
+    axios
+      .delete(`${API}/songs/${id}`)
+      .then(() => {
+        navigate(`/songs`);
+      })
+      .catch((e) => {
+        console.warn("catch:", e);
+      });
+  };
+
+return (
+  <article>
+    <h1>{song.is_favorite ? <span>⭐️</span> : null} {song.name} - By {song.artist}</h1>
+    <h2>
+      <span>
+        <a href={song.album}></a>
+      </span>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {song.album}
+    </h2>
+    <h3>Time: {song.time}</h3>
+    <div className="showNavigation">
+      <div>
+        <Link to={`/songs`}>
+          <button>Back</button>
+        </Link>
+      </div>
+      <div>
+        <Link to={`/songs/id/edit`}>
+          <button>Edit</button>
+        </Link>
+      </div>
+      <div>
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+    </div>
+
+  </article>
+ ) 
+}
+
+export default SongDetails;
