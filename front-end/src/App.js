8db6485f676a0cca691ba7607dbcaf7185e5acb2
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-29 01:55:52 -0400

    completed all Pages/connection to their specific Component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // PAGES
-// import Edit from "./Pages/Edit";
-// import FourOFour from "./Pages/FourOFour";
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
 import Home from "./Pages/Home";
-// import Index from "./Pages/Index";
-// import New from "./Pages/New";
-// import Show from "./Pages/Show";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
 
 // COMPONENTS
 import NavBar from "./Components/NavBar";
@@ -21,11 +21,11 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/songs" element={<Index />} />
+            <Route path="/songs" element={<Index />} />
             <Route path="/songs/new" element={<New />} />
             <Route exact path="/songs/:id" element={<Show />} />
             <Route path="/songs/:id/edit" element={<Edit />} />
-            <Route path="*" element={<FourOFour />} /> */}
+            <Route path="*" element={<FourOFour />} />
           </Routes>
         </main>
       </Router>
