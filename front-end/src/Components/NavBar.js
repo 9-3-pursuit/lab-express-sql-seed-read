3cab5b5158f257bf9b6f59a888121f6eded0d97b
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-29 03:19:51 -0400

    modified Tuner to all Caps (TUNER) in NavBar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -4,10 +4,11 @@ export default function NavBar() {
   return (
     <nav>
       <h1>
-        <Link to="/songs">Songs</Link>
+        <Link to="/songs">TUNER</Link>
       </h1>
       <button>
-        <Link to="/songs/new">New Song</Link>
+      
+        <Link  to="/songs/new">New Song</Link>
       </button>
     </nav>
   );
