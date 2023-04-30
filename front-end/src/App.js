45f2b1163abe1195dbd0a455fd9b6f06ca4b2bde
Keeanu Rodriguez <keeanu7@gmail.com>
2023-04-30 19:32:04 -0400

    index test pass

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,23 @@
+import {Route, BrowserRouter, Routes} from "react-router-dom"
+import Home from "./components/Home.jsx"
+import Index from "./components/Index.jsx"
+import Show from "./components/Index.jsx"
+import Edit from "./components/Edit.jsx"
+import New from "./components/New.jsx"
+import Nav from "./components/Nav.jsx"
+
 function App() {
   return (
-    <div className="">
-      <h1>Tuner App</h1>
-    </div>
+    <BrowserRouter>
+    <Nav/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/new" element={<New/>}/>
+      <Route path="/songs" element={<Index/>}/>
+      <Route path="/songs/:index" element={<Show/>}/>
+      <Route path="/songs/:index/edit" element={<Edit/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
