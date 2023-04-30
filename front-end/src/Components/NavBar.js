e6ad7a7e2d31daace8e9ea183ac6b846377d4cac
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-30 14:29:43 -0400

    modified NavBar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav>
-      <h1>
-        <Link to="/songs">Tuner</Link>
-      </h1>
+      <Link to="/songs"><h1><span>Tuner</span></h1></Link>
+      <br/>
       <button>
-      
         <Link  to="/songs/new">New Song</Link>
       </button>
     </nav>
