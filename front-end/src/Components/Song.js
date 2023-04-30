e306e644b669da7f16809fbec47e93ed8dcf1042
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-30 16:00:31 -0400

    modified a href to have a link to each song's show page

--- front-end/src/Components/Song.js
+++ front-end/src/Components/Song.js
@@ -2,18 +2,16 @@ import { Link } from "react-router-dom";
 
 function Song({ song }) {
   return (
-    <tr>
-      <td>
+    <tr className='Song'>
+      <td className="td">
         {song.is_favorite ? (
           <span>⭐️</span>
         ) : (
           <span>&nbsp; &nbsp; &nbsp;</span>
         )}
       </td>
-      <td>
-        <a href={song.url} target="_blank" rel="noreferrer">
-          {song.name}
-        </a>
+      <td className="td">
+        <a href={`/songs/${song.id}`}>{song.name}</a> 
       </td>
       <td>
         <Link to={`/songs/${song.id}`}>{song.artist}</Link>      
