4af985c7fdd76002290bf1ec4009c4ca19d3fba7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-30 00:16:00 -0400

    installed tailwind success

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,12 +13,12 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/songs" element={<Index />} />
-            <Route path="/songs/new" element={<New />} />
+            {/* <Route path="/songs" element={<Index />} /> */}
+            {/* <Route path="/songs/new" element={<New />} /> */}
           </Routes>
         </main>
       </BrowserRouter>
@@ -29,17 +29,53 @@ function App() {
 export default App;
 
 /*
+
+// DEPENDENCIES
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import AboutUs from "./Components/AboutUs";
+import DarkMode from "./Components/DarkMode";
+
+// PAGES
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
 import Show from "./Pages/Show";
 import Edit from "./Pages/Edit";
 import FourOFour from "./Pages/FourOFour";
 import ContactUs from "./Components/ContactUs";
-<Route path="/" element={<Home />} />
-<Route path="/about" element={<AboutUs />} />
-<Route path="/contact" element={<ContactUs />} />
-<Route path="/budgets/:index/edit" element={<Edit />} />
-<Route path="/budgets/new" element={<New />} />
-<Route path="/budgets/:index" element={<Show />} />
-<Route path="/budgets" element={<Index />} />
-<Route path="*" element={<FourOFour />} />
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <DarkMode>
+          <Header />
+          <NavBar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/budgets/:index/edit" element={<Edit />} />
+              <Route path="/budgets/new" element={<New />} />
+              <Route path="/budgets/:index" element={<Show />} />
+              <Route path="/budgets" element={<Index />} />
+              <Route path="*" element={<FourOFour />} />
+            </Routes>
+          </main>
+          <Footer />
+        </DarkMode>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
 
 */
--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="flex flex-col items-center m-6 p-6 justify-center">
-      <h1 className="text-4xl font-bold mb-6">Tuner App</h1>
+      <h1 className="text-4xl font-bold mb-6 underline">Tuner App</h1>
     </div>
   );
 }
