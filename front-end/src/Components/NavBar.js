565bbd6f5869819278bed9c5e46dc9f9be7bf14a
joshuanelsondev <joshuanelsondev@gmail.com>
2023-05-01 00:58:45 -0400

    Edit NavBar.js

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,9 +1,38 @@
 import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaHeadphones,
+  FaLaptop,
+  FaPlus,
+  FaLayerGroup,
+} from "react-icons/fa";
 
 export default function NavBar() {
     return (
+      <div className="flex flex-col bg-neutral w-28 h-full text-center text-light pt-8">
         <div>
-            <Link to={"/songs"} />
+          <Link className="flex flex-col" to={"/songs"}>
+            <FaHeadphones
+              className="self-center cursor-pointer mb-8  hover:text-secondary"
+              size={30}
+            />
+          </Link>
+          <hr className="mb-4" />
         </div>
-    )
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center justify-start pl-2 gap-2 group">
+            <FaLaptop className="group-hover:text-secondary" />
+            <Link to={"/"}>Home</Link>
+          </div>
+          <div className="flex items-center justify-start pl-2 gap-2 group">
+            <FaLayerGroup className="group-hover:text-secondary" />
+            <Link to={"/songs"}>My List</Link>
+          </div>
+          <div className="flex items-center justify-start pl-2 gap-2 group">
+            <FaPlus className="group-hover:text-secondary" />
+            <Link to={"/songs/new"}>Add Song</Link>
+          </div>
+        </div>
+      </div>
+    );
 }
