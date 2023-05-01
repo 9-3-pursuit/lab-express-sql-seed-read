7bae39facae30c4b4e5149ed17148b82dd149ddd
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-04-30 20:49:20 -0400

    corrects base routes to jason to avoid errors

--- back-end/app.js
+++ back-end/app.js
@@ -8,13 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to Tuner");
+  res.status(200).json("Welcome to Tuner");
 });
 
 app.use("/songs", songsRouter);
 
 app.get("*", (req, res) => {
-  res.status(400).send("Resource could not be found");
+  res.status(400).json("Resource could not be found");
 });
 
 module.exports = app;
