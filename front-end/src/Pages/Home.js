1d97ac90e79f651b31f30f635c69672f9a73e095
touhami-ben <brooklyn.ben76@gmail.com>
2023-05-02 13:33:59 -0400

    index and show pages

---
+++ front-end/src/Pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Tuner App</h1>
+        Select the link above to see a list of your favorite songs
+      </div>
+    );
+  };
+  
