ded8e8053a1ecac6d359bb41c2d171c080aba5d4
amber-king <amberking@pursuit.org>
2023-05-04 15:30:32 -0400

    added artist query

---
+++ back-end/queries/artist.js
@@ -0,0 +1,70 @@
+const db = require("../db/dbConfig.js");
+
+const getAllArtist = async () => {
+  try {
+    const allArtist = await db.any("SELECT * FROM songs");
+    return allArtist;
+  } catch (error) {
+    return { error: error };
+  }
+};
+
+const getArtist = async (id) => {
+  console.log(id);
+  try {
+    const artist = await db.one(`SELECT * FROM songs WHERE id=${id}`);
+    return artist;
+  } catch (error) {
+    return { error: error };
+  }
+};
+
+const createArtist = async (song) => {
+  try {
+    const newArtist = await db.one(
+      `INSERT INTO
+        song(name, artist, album ,time, is_favorite)
+       VALUES
+        ($1, $2, $3, $4, $5)
+       RETURNING *;`,
+      [song.name, song.artist, song.album, song.time, song.is_favorite]
+    );
+    return newArtist;
+  } catch (error) {
+    return { error: error };
+  }
+};
+
+const deleteArtist = async (id) => {
+  //songs/id
+  try {
+    const deletedArtist = await db.one(
+      "DELETE FROM songs WHERE id=$1 RETURNING *",
+      id
+    );
+    return deletedArtist;
+  } catch (e) {
+    return e;
+  }
+};
+
+const updateArtist = async (id, song) => {
+  // songs/id
+  try {
+    const updatedArtist = await db.one(
+      `UPDATE songs SET name=$1, artist=$2, album=$3, time=$4 is_favorite=$6 WHERE id=$6 RETURNING *`,
+      [song.name, song.artist, song.album, song.time,song.is_favorite]
+    );
+    return updatedArtist;
+  } catch (e) {
+    return e;
+  }
+};
+
+module.exports = {
+  getAllArtist,
+  getArtist,
+  createArtist,
+  deleteArtist,
+  updateArtist,
+};
